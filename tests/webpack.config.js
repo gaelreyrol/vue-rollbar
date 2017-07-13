e6e94f8df38665fd873ef9d2f5c8c003dd8aabaf
Gaël Reyrol <gael@soyhuce.fr>
2017-07-13 09:47:59 +0200

    feat(tests): Implement a simple webpack template to easily test rollbar integration

---
+++ tests/webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path')
+var webpack = require('webpack')
+var dotenv = require('dotenv')
+
+dotenv.config()
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'ROLLBAR_CLIENT_TOKEN': JSON.stringify(process.env.ROLLBAR_CLIENT_TOKEN)
+    })
+  ]
+}
