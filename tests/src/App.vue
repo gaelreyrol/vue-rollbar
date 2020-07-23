f9e4c96ccbff663857576f0721ff847acd407b18
Matt Boyd <matt.boyd@moneysupermarket.com>
2020-07-23 09:42:31 +0100

    trim fat from standard app

--- tests/src/App.vue
+++ tests/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1></h1>
     <h2>Essential Links</h2>
     <ul>
@@ -57,24 +56,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
